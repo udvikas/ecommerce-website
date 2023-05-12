b01bef05e881b4a709779a8a177549c6112670fd
Vikash Sharma <kvikashsharma09@gmail.com>
2023-05-12 21:04:47 +0530

    fifth commit(built home page)

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { Fragment } from "react";
+import pic from '../asset/pic.jpg'
+import './AboutPage.css';
 
 const AboutPage = () => {
-  return <h1>About Page</h1>;
+  return (
+    <Fragment>
+      {" "}
+      <h1>Who We Are</h1>
+      <div className="settle">
+        <img src={pic} alt="banner" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
+          harum dolor voluptatem mollitia eos eius, facere laboriosam ullam
+          maiores aspernatur velit nulla quia magnam obcaecati dignissimos
+          accusamus aut ab? Debitis quae possimus est aliquid sint dolor
+          adipisci, voluptatum, perferendis excepturi vitae ducimus eligendi
+          enim atque voluptate! Neque magni esse illum nemo unde suscipit labore
+          atque ratione ullam nostrum nulla deleniti necessitatibus quae
+          voluptas quas, pariatur sit rem. Iusto natus impedit itaque dolorum
+          ipsa nostrum porro placeat, beatae ab voluptatem consequatur
+          exercitationem illo, deleniti perspiciatis laudantium velit, vero
+          veniam sint obcaecati alias explicabo? Fugiat nobis minima laboriosam
+          sapiente cumque, vero accusantium, quidem, facilis aut a totam
+          temporibus quia vel maxime. Nostrum, tenetur libero? Aut et, ea
+          accusantium, impedit aliquam commodi voluptate repellendus earum
+          inventore magnam neque delectus molestiae, tempora voluptatem nihil
+          quia! Numquam illo explicabo dolorem nam, blanditiis laborum enim sint
+          fuga! Voluptate impedit eos, quis maiores architecto consequatur
+          excepturi temporibus molestiae deleniti quidem sed iusto veniam fugiat
+          natus rerum. Quam, saepe impedit? Hic doloribus exercitationem odio
+          delectus ad quasi sequi reprehenderit vel debitis, totam magnam
+          repellendus facilis fugit consequatur illo autem error ab earum? Sed
+          modi possimus minima quaerat delectus quo. A quod neque iste assumenda
+          recusandae similique modi fugit praesentium architecto. Dolore fugit
+          debitis repellat quas incidunt labore ea ipsa! Quod, eius,
+          perspiciatis modi explicabo unde repellendus dicta libero eos velit
+          quia rem quidem eum iusto nemo earum omnis distinctio inventore.
+         
+        </p>
+      </div>
+    </Fragment>
+  );
 };
 export default AboutPage;
-
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,69 @@
 import React from "react";
-
+import "./HomePage.css";
+import { Table, Button } from "react-bootstrap";
 const Homepage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Tour Details</h1>
+      <Table striped bordered hover variant="dark">
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>JUL 16</td>
+            <td>DETROIT, MI</td>
+            <td>DTE ENERGY MUSIC THEATRE</td>
+            <td>
+              <Button variant="info">Buy Tickets</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>JUL 19</td>
+            <td>TORONTO,ON</td>
+            <td>BUDWEISER STAGE</td>
+            <td>
+              <Button variant="info">Buy Tickets</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>JUL 22</td>
+            <td>BRISTOW, VA</td>
+            <td>JIGGY LUBE LIVE</td>
+            <td>
+              <Button variant="info">Buy Tickets</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>JUL 29</td>
+            <td>PHOENIX, AZ</td>
+            <td>AK-CHIN PAVILION</td>
+            <td>
+              <Button variant="info">Buy Tickets</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>AUG 2</td>
+            <td>LAS VEGAS, NV</td>
+            <td>T-MOBILE ARENA</td>
+            <td>
+              <Button variant="info">Buy Tickets</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>AUG 7</td>
+            <td>CONCORD, CA</td>
+            <td>CONCORD PAVILION</td>
+            <td>
+              <Button variant="info">Buy Tickets</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 };
 export default Homepage;
